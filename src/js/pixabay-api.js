cc1ac310a78fa18b18b60c7ee79278fea346ac1e
MarynaBinko <benyhorses@hotmail.com>
2024-04-12 17:15:27 +0100

    homework 12 fixed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = "43197174-dcc5f5044572d8f441379a766";
 const per_page = 15;
 
-export async function searchPictures(searchQuery, page) {
+async function searchPictures(searchQuery){
     const params = new URLSearchParams({
         key: API_KEY,
         q: searchQuery,
@@ -13,7 +13,11 @@ export async function searchPictures(searchQuery, page) {
         per_page: per_page,
         page: page
     });
-
+    
     const response = await axios.get(`https://pixabay.com/api/?${params}`);
-    return response.data;
+console.log(response);
+    const { hits, totalHits } = response.data; 
+    const totalPages = Math.ceil(totalHits / per_page);    
+    return { hits, totalHits, totalPages };
+    
 }
